d03ef49fc335daf7d933a6db6e6eeb72e4fbb22a
MikeLangford <mgl2nd@gmail.com>
2011-01-10 23:36:43 -0500

    Initial commit

--- Gemfile
+++ Gemfile
@@ -26,9 +26,6 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
-# group :development, :test do
-#   gem 'webrat'
-# end
 
 group :development do
 gem 'rspec-rails', '2.3.0'
@@ -36,5 +33,5 @@ end
 
 group :test do
 gem 'rspec', '2.3.0'
-gem 'webrat', '0.7.1'
+gem 'webrat'
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '2755bfcec7d1a8ad16d5945bcad7f6552f36d16a4df9dc211018625128f2098937bbdf369b401398bc39641da69121e4d1bd2ef97d29e59a9c78c2cd420a649f'
+SampleApp::Application.config.secret_token = 'de3ec483146863c44b6b50b8e334bafe1da474f7269c699e53089264b67525a116e63d68468ac676e944f2e761a562cd3759b7f79c7eb21b928ae7d4c8934870'
